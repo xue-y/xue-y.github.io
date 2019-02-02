604f8d711606470b3373293222a52b7d51ac576d
雪剑无影 <php.develop@qq.com>
2019-02-02 15:46:13 +0800

    Add files via upload
    上传前端网页常用JS 效果

---
+++ fore-end/js/t_tween.js
@@ -0,0 +1,229 @@
+// 时间版
+
+/* 调用示例
+btn.onclick = function(){
+   move(test1,{width:300,height:200},500,'Bounce-easeInOut',function(){
+    move(test2,{width:300},500,'Linear');
+   });
+   move(test2,{left:400},500,'Linear');
+   
+} */
+// 关于透明度 传值时 传的是 1---100 之间
+function move(obj,json,times,fx,fn){
+  //获取当前毫秒数
+  var startTime = +new Date();  
+  /*获取初始值*/
+  var iCur = {};
+  for(var attr in json){
+    if(attr == 'opacity'){
+      //对当前值的取值进行四舍五入，去除由于javascript小数计数中的bug存在的小尾巴
+      iCur[attr] = Math.round(getCSS(obj,attr)*100);
+    }else{
+      //去掉单位
+      iCur[attr] = parseInt(getCSS(obj,attr));
+    }
+  }
+  //清除定时器
+  cancelAnimationFrame(obj.timer);
+  obj.timer = requestAnimationFrame(function func()
+  {
+    //获取t、d参数
+    var d = times;
+    var t = d - Math.max(0,startTime - (+new Date()) + d);
+	var fxArr = fx.split('.');
+    for(var attr in json)
+	{
+      /*获取b、c、p这三个参数*/
+      var b = iCur[attr];
+      var c = json[attr]-iCur[attr];
+  
+      if(fxArr.length == 2){
+        var p = Tween[fxArr[0]][fxArr[1]](t,b,c,d);
+      }else{
+        var p = Tween[fx](t,b,c,d);
+      }     
+      /*赋值操作*/
+      if(attr == 'opacity'){
+        obj.style.opacity = p / 100;
+        obj.style.filter = 'alpha(opacity=' + p + ')';
+      }else{
+        obj.style[attr] = p + 'px';
+      }  
+    }
+      obj.timer = requestAnimationFrame(func);
+      /*运行指定时间后*/
+      if(t == d){
+        //清除定时器
+        cancelAnimationFrame(obj.timer);
+        //设置回调函数
+        fn && fn.call(obj);        
+      }    
+  });
+} 
+
+if (!window.requestAnimationFrame) {
+    requestAnimationFrame = function(fn) {
+        setTimeout(fn, 17);
+    };    
+}
+if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+    };
+}
+function getCSS(obj,style){
+    if(window.getComputedStyle){
+        return getComputedStyle(obj,false)[style];
+    }
+    return obj.currentStyle[style]; 
+} 
+
+function css(t, n) {
+    return t.currentStyle ? t.currentStyle[n] : getComputedStyle(t, !1)[n]
+}
+
+// Tween类
+var Tween = {
+    Linear: function(t,b,c,d){ return c*t/d + b; },
+    Quad: {
+        easeIn: function(t,b,c,d){
+            return c*(t/=d)*t + b;
+        },
+        easeOut: function(t,b,c,d){
+            return -c *(t/=d)*(t-2) + b;
+        },
+        easeInOut: function(t,b,c,d){
+            if ((t/=d/2) < 1) return c/2*t*t + b;
+            return -c/2 * ((--t)*(t-2) - 1) + b;
+        }
+    },
+    Cubic: {
+        easeIn: function(t,b,c,d){
+            return c*(t/=d)*t*t + b;
+        },
+        easeOut: function(t,b,c,d){
+            return c*((t=t/d-1)*t*t + 1) + b;
+        },
+        easeInOut: function(t,b,c,d){
+            if ((t/=d/2) < 1) return c/2*t*t*t + b;
+            return c/2*((t-=2)*t*t + 2) + b;
+        }
+    },
+    Quart: {
+        easeIn: function(t,b,c,d){
+            return c*(t/=d)*t*t*t + b;
+        },
+        easeOut: function(t,b,c,d){
+            return -c * ((t=t/d-1)*t*t*t - 1) + b;
+        },
+        easeInOut: function(t,b,c,d){
+            if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+            return -c/2 * ((t-=2)*t*t*t - 2) + b;
+        }
+    },
+    Quint: {
+        easeIn: function(t,b,c,d){
+            return c*(t/=d)*t*t*t*t + b;
+        },
+        easeOut: function(t,b,c,d){
+            return c*((t=t/d-1)*t*t*t*t + 1) + b;
+        },
+        easeInOut: function(t,b,c,d){
+            if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+            return c/2*((t-=2)*t*t*t*t + 2) + b;
+        }
+    },
+    Sine: {
+        easeIn: function(t,b,c,d){
+            return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+        },
+        easeOut: function(t,b,c,d){
+            return c * Math.sin(t/d * (Math.PI/2)) + b;
+        },
+        easeInOut: function(t,b,c,d){
+            return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+        }
+    },
+    Expo: {
+        easeIn: function(t,b,c,d){
+            return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+        },
+        easeOut: function(t,b,c,d){
+            return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+        },
+        easeInOut: function(t,b,c,d){
+            if (t==0) return b;
+            if (t==d) return b+c;
+            if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        }
+    },
+    Circ: {
+        easeIn: function(t,b,c,d){
+            return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+        },
+        easeOut: function(t,b,c,d){
+            return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+        },
+        easeInOut: function(t,b,c,d){
+            if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+            return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+        }
+    },
+    Elastic: {
+        easeIn: function(t,b,c,d,a,p){
+            if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+            if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+            else var s = p/(2*Math.PI) * Math.asin (c/a);
+            return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+        },
+        easeOut: function(t,b,c,d,a,p){
+            if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+            if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+            else var s = p/(2*Math.PI) * Math.asin (c/a);
+            return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b);
+        },
+        easeInOut: function(t,b,c,d,a,p){
+            if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+            if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+            else var s = p/(2*Math.PI) * Math.asin (c/a);
+            if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+            return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+        }
+    },
+    Back: {
+        easeIn: function(t,b,c,d,s){
+            if (s == undefined) s = 1.70158;
+            return c*(t/=d)*t*((s+1)*t - s) + b;
+        },
+        easeOut: function(t,b,c,d,s){
+            if (s == undefined) s = 1.70158;
+            return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+        },
+        easeInOut: function(t,b,c,d,s){
+            if (s == undefined) s = 1.70158; 
+            if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+            return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+        }
+    },
+    Bounce: {
+        easeIn: function(t,b,c,d){
+            return c - Tween.Bounce.easeOut(d-t, 0, c, d) + b;
+        },
+        easeOut: function(t,b,c,d){
+            if ((t/=d) < (1/2.75)) {
+                return c*(7.5625*t*t) + b;
+            } else if (t < (2/2.75)) {
+                return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+            } else if (t < (2.5/2.75)) {
+                return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+            } else {
+                return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+            }
+        },
+        easeInOut: function(t,b,c,d){
+            if (t < d/2) return Tween.Bounce.easeIn(t*2, 0, c, d) * .5 + b;
+            else return Tween.Bounce.easeOut(t*2-d, 0, c, d) * .5 + c*.5 + b;
+        }
+    }
+};  
